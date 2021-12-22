e4c10b19fedb4e537688c6f24e166f3eb8074de8
trancuongitpro <cuongtmth2008049@fpt.edu.vn>
2021-12-22 14:51:23 +0700

    Add project files.

---
+++ Data/MvcBooksContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class MvcBooksContext : DbContext
+    {
+        public MvcBooksContext(DbContextOptions<MvcBooksContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
